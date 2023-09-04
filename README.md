e2752afcc922e9416dd0c70b27503d744b17c05c
Oliver Tacke <o.tacke@posteo.de>
2023-09-04 14:17:36 +0200

    Add initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+H5P Editor Phrase Randomizer Textual Editor
+==========
+Makes it fast and easy to create Phrase Randomizer segments through text input.
---
+++ src/entries/dist.js
@@ -0,0 +1,5 @@
+import '@styles/h5peditor-phrase-randomizer-textual-editor.scss';
+import PhraseRandomizerTextualEditor from '@scripts/h5peditor-phrase-randomizer-textual-editor.js';
+
+// Load library
+H5PEditor.PhraseRandomizerTextualEditor = PhraseRandomizerTextualEditor;
